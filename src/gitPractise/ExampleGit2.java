60fac74aee71b8a2aa98704d00c6cd81f6a23f4a
chand <chand@LAPTOP-NGNI4STN>
2024-02-16 15:01:52 +0530

    Hey i am commitng the new code with branchx

---
+++ src/gitPractise/ExampleGit2.java
@@ -0,0 +1,15 @@
+package gitPractise;
+
+public class ExampleGit2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		ExampleGit2 git1 = new ExampleGit2();
+		git1.GitEx();
+		
+	}
+	public void GitEx() {
+		System.out.println("Hey this is git method");
+	}
+
+}
