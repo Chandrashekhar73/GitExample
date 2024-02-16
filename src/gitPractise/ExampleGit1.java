1aaccd138163640bb79d1dfc798699790ff15d88
chand <chand@LAPTOP-NGNI4STN>
2024-02-16 08:27:28 +0530

    Hey this git commit...

---
+++ src/gitPractise/ExampleGit1.java
@@ -0,0 +1,15 @@
+package gitPractise;
+
+public class ExampleGit1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		ExampleGit1 git1 = new ExampleGit1();
+		git1.GitEx();
+		
+	}
+	public void GitEx() {
+		System.out.println("Hey this is git method");
+	}
+
+}
